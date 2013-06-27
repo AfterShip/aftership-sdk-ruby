561b8da71cc149eaf146325e6c8822b2802ed024
LU Wenlong <a88i99@gmail.com>
2013-06-27 15:34:34 +0800

    clean up

--- test/test.rb
+++ test/test.rb
@@ -5,6 +5,6 @@ AfterShipAPI.api_key = "YOUR_API_KEY"
 
 
 # call
-puts slug_name_pairs = AfterShipAPI::Courier.get_couriers
+puts slug_name_pairs = AfterShipAPI::V2::Courier.get_couriers
 puts AfterShipAPI::V2::Tracking.create("218501627271", "toll-global-express")
 puts AfterShipAPI::V2::Tracking.get("218501627271", "toll-global-express")
