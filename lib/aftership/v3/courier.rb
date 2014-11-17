2f1ff5247336e74f31bf1b307e4b649536f2723c
Laurynas Butkus <laurynas.butkus@gmail.com>
2014-11-17 18:30:18 +0200

    Fix indentation

--- lib/aftership/v3/courier.rb
+++ lib/aftership/v3/courier.rb
@@ -1,21 +1,21 @@
 require File.dirname(__FILE__) + '/base'
 
 module AfterShip
-	module V3
-		class Courier < AfterShip::V3::Base
+  module V3
+    class Courier < AfterShip::V3::Base
 
-			def self.get
-				new(:get, 'couriers').call
-			end
+      def self.get
+        new(:get, 'couriers').call
+      end
 
-			def self.detect(tracking_number = '')
-				if tracking_number.empty?
-					raise ArgumentError.new('tracking_number is required.')
-				else
-					new(:get, "couriers/detect/#{tracking_number}").call
-				end
-			end
+      def self.detect(tracking_number = '')
+        if tracking_number.empty?
+          raise ArgumentError.new('tracking_number is required.')
+        else
+          new(:get, "couriers/detect/#{tracking_number}").call
+        end
+      end
 
-		end
-	end
+    end
+  end
 end
--- lib/aftership/v4/tracking.rb
+++ lib/aftership/v4/tracking.rb
@@ -1,72 +1,72 @@
 require File.dirname(__FILE__) + '/base'
 
 module AfterShip
-	module V4
-		class Tracking < AfterShip::V4::Base
+  module V4
+    class Tracking < AfterShip::V4::Base
 
-			#POST /trackings
-			def self.create(tracking_number, params = {})
-				if tracking_number.empty?
-					raise ArgumentError.new('tracking_number is required.')
-				else
-					query_hash = {:tracking_number => tracking_number}
-					query_hash.merge!(params)
-					body = {:tracking => query_hash}
-					new(:post, 'trackings', {}, body).call
-				end
-			end
+      #POST /trackings
+      def self.create(tracking_number, params = {})
+        if tracking_number.empty?
+          raise ArgumentError.new('tracking_number is required.')
+        else
+          query_hash = { :tracking_number => tracking_number }
+          query_hash.merge!(params)
+          body = { :tracking => query_hash }
+          new(:post, 'trackings', {}, body).call
+        end
+      end
 
-			#POST /trackings/:slug/:tracking_number/retrack
-			def self.retrack(slug, tracking_number)
-				if slug.empty? || tracking_number.empty?
-					raise ArgumentError.new('slug and tracking_number are required.')
-				end
-				new(:post, "trackings/#{slug}/#{tracking_number}/retrack").call
-			end
+      #POST /trackings/:slug/:tracking_number/retrack
+      def self.retrack(slug, tracking_number)
+        if slug.empty? || tracking_number.empty?
+          raise ArgumentError.new('slug and tracking_number are required.')
+        end
+        new(:post, "trackings/#{slug}/#{tracking_number}/retrack").call
+      end
 
-			#DELETE /trackings/:slug/:tracking_number
-			def self.delete(slug, tracking_number)
-				if slug.empty? || tracking_number.empty?
-					raise ArgumentError.new('slug and tracking_number are required.')
-				end
-				new(:delete, "trackings/#{slug}/#{tracking_number}").call
-			end
+      #DELETE /trackings/:slug/:tracking_number
+      def self.delete(slug, tracking_number)
+        if slug.empty? || tracking_number.empty?
+          raise ArgumentError.new('slug and tracking_number are required.')
+        end
+        new(:delete, "trackings/#{slug}/#{tracking_number}").call
+      end
 
-			#GET /trackings/:slug/:tracking_number
-			def self.get(slug, tracking_number, params = {})
-				if slug.empty? || tracking_number.empty?
-					raise ArgumentError.new('slug and tracking_number are required.')
-				end
-				new(:get, "trackings/#{slug}/#{tracking_number}", params).call
-			end
+      #GET /trackings/:slug/:tracking_number
+      def self.get(slug, tracking_number, params = {})
+        if slug.empty? || tracking_number.empty?
+          raise ArgumentError.new('slug and tracking_number are required.')
+        end
+        new(:get, "trackings/#{slug}/#{tracking_number}", params).call
+      end
 
-			#GET /trackings/:id
-			def self.get_by_id(id, params = {})
-				if id.empty?
-					raise ArgumentError.new('id is required.')
-				end
-				new(:get, "trackings/#{id}", params).call
-			end
+      #GET /trackings/:id
+      def self.get_by_id(id, params = {})
+        if id.empty?
+          raise ArgumentError.new('id is required.')
+        end
+        new(:get, "trackings/#{id}", params).call
+      end
 
-			#GET /trackings
-			def self.get_all(params = {})
-				new(:get, 'trackings', params).call
-			end
+      #GET /trackings
+      def self.get_all(params = {})
+        new(:get, 'trackings', params).call
+      end
 
-			#PUT /trackings/:slug/:tracking_number
-			def self.update(slug, tracking_number, params = {})
-				if slug.empty? || tracking_number.empty?
-					raise ArgumentError.new('slug and tracking_number are required.')
-				end
-				body = {:tracking => params}
-				new(:put, "trackings/#{slug}/#{tracking_number}", {}, body).call
-			end
+      #PUT /trackings/:slug/:tracking_number
+      def self.update(slug, tracking_number, params = {})
+        if slug.empty? || tracking_number.empty?
+          raise ArgumentError.new('slug and tracking_number are required.')
+        end
+        body = { :tracking => params }
+        new(:put, "trackings/#{slug}/#{tracking_number}", {}, body).call
+      end
 
-			#Deprecated
-			#POST /trackings/:slug/:tracking_number/reactivate
-			def self.reactivate(slug, tracking_number)
-				raise StandardError.new('This method is deprecated, please use "retrack" instead')
-			end
-		end
-	end
+      #Deprecated
+      #POST /trackings/:slug/:tracking_number/reactivate
+      def self.reactivate(slug, tracking_number)
+        raise StandardError.new('This method is deprecated, please use "retrack" instead')
+      end
+    end
+  end
 end
