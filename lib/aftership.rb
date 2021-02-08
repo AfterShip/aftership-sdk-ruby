45f917af2bbaf547c42236a6dea147d990906840
Bossa Wong <bossa@aftership.com>
2021-02-08 18:26:35 +0800

    Revert "Revert "update sdk to 4.5.0 with required ruby 2.3.0+""

--- lib/aftership.rb
+++ lib/aftership.rb
@@ -5,9 +5,10 @@ require 'aftership/v4/tracking'
 require 'aftership/v4/last_checkpoint'
 
 module AfterShip
-  class << self;
+  class << self
     attr_accessor :api_key
   end
 
   URL = ENV['AFTERSHIP_API_ENDPOINT'] || 'https://api.aftership.com'
+  VERSION = '4.5.0'
 end
